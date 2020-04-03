f12bac2db547cd53f9864194aa29bfbabdbf2423
Daniel Haase <daniel.haase@zeiss.com>
2020-04-03 12:30:22 +0200

    Create basic setup.py for package installation and wheel building

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="bsconv",
+    version="0.0.3",
+    description="Reference implementation for Blueprint Separable Convolutions (BSConv)",
+    url="https://github.com/zeiss-microscopy/BSConv",
+    packages=setuptools.find_packages(),
+)
