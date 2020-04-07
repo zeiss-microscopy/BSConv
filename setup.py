d8df4d49a173afd6ca2db934468200fee1b00bb6
Daniel Haase <daniel.haase@zeiss.com>
2020-04-07 15:41:22 +0200

    Add license

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setuptools.setup(
     version="0.0.4",
     description="Reference implementation for Blueprint Separable Convolutions (BSConv)",
     url="https://github.com/zeiss-microscopy/BSConv",
+    license="BSD 3-Clause Clear License",
     packages=setuptools.find_packages(),
 )
