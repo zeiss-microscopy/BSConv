2a43229ede2de974192fcb7a27c2746af94bee2a
Daniel Haase <daniel.haase@zeiss.com>
2020-04-08 13:06:46 +0200

    Bump version to 0.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="bsconv",
-    version="0.0.5",
+    version="0.0.6",
     description="Reference implementation for Blueprint Separable Convolutions (BSConv)",
     url="https://github.com/zeiss-microscopy/BSConv",
     license="BSD 3-Clause Clear License",
