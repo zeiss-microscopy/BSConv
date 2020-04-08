ff16b9399a53a794be56fc0d1c6b1e8a8aa17030
Daniel Haase <daniel.haase@zeiss.com>
2020-04-08 14:27:55 +0200

    Include version number in package and adapt setup.py script to use that number

--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
+import os.path
+import re
+
 import setuptools
 
+
+package_name = "bsconv"
+
+
+def get_version():
+    version_filename = os.path.join(os.path.abspath(os.path.dirname(__file__)), package_name, "__init__.py")
+    with open(version_filename, "r") as f:
+        for line in f:
+            line = line.strip()
+            match = re.match(r"^__version__ = \"([^\"]*)\"", line)
+            if match is not None:
+                return match.group(1)
+    raise RuntimeError("Could not find version string in file ''".format(version_filename))
+    
+
 setuptools.setup(
-    name="bsconv",
-    version="0.0.7",
+    name=package_name,
+    version=get_version(),
     description="Reference implementation for Blueprint Separable Convolutions (BSConv)",
     url="https://github.com/zeiss-microscopy/BSConv",
     license="BSD 3-Clause Clear License",
