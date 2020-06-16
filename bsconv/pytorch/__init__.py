22bc568e74dcee59227ea85de85c596c86b81b74
Daniel Haase <daniel.haase@zeiss.com>
2020-06-16 10:03:09 +0200

    Make model provider accessible from 'bsconv.pytorch'

--- bsconv/pytorch/__init__.py
+++ bsconv/pytorch/__init__.py
@@ -1,3 +1,11 @@
-from bsconv.pytorch.modules import BSConvU, BSConvS, BSConvS_ModelRegLossMixin
+# ready-to-use BSConv model definitions
+from bsconv.pytorch.provider import get_model
+
+# BSConv as general drop-in replacement
 from bsconv.pytorch.replacers import BSConvU_Replacer, BSConvS_Replacer
+
+# BSConv PyTorch modules
+from bsconv.pytorch.modules import BSConvU, BSConvS, BSConvS_ModelRegLossMixin
+
+# model profiler for measuring parameter and FLOP counts
 from bsconv.pytorch.profile import ModelProfiler
