af88840f995b385d31758b646b1498b703ea4728
Daniel Haase <daniel.haase@zeiss.com>
2020-06-16 09:49:28 +0200

    Make setup script scan for and install scripts from the bin directory

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import glob
 import os.path
 import re
 
@@ -7,8 +8,13 @@ import setuptools
 package_name = "bsconv"
 
 
+package_dir = os.path.abspath(os.path.dirname(__file__))
+source_dir = os.path.join(package_dir, package_name)
+bin_dir = os.path.join(package_dir, "bin")
+
+
 def get_version():
-    version_filename = os.path.join(os.path.abspath(os.path.dirname(__file__)), package_name, "__init__.py")
+    version_filename = os.path.join(source_dir, "__init__.py")
     with open(version_filename, "r") as f:
         for line in f:
             line = line.strip()
@@ -16,6 +22,13 @@ def get_version():
             if match is not None:
                 return match.group(1)
     raise RuntimeError("Could not find version string in file ''".format(version_filename))
+
+
+def get_bin_filenames(extensions=("py", "sh")):
+    filenames = []
+    for extension in extensions:
+        filenames += list(glob.glob(os.path.join(bin_dir, "*.{}".format(extension))))
+    return filenames
     
 
 setuptools.setup(
@@ -25,4 +38,5 @@ setuptools.setup(
     url="https://github.com/zeiss-microscopy/BSConv",
     license="BSD 3-Clause Clear License",
     packages=setuptools.find_packages(),
+    scripts=get_bin_filenames(),
 )
